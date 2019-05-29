637a46ccee61daa18daf0fc4c8d00618123eb03e
Tamara Torres <tammy.torres.88@gmail.com>
2019-05-29 08:43:45 -0300

    adding custom changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import {HeroesComponent} from './heroes/heroes.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+import {HeroSearchComponent} from './hero-search/hero-search.component';
+import {Messages, MessagesComponent } from './messages/messages.component';
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent},
   { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  { path: 'detail/:id', component: HeroDetailComponent}
+  { path: 'detail/:id', component: HeroDetailComponent},
+  { path: 'search', component: HeroSearchComponent},
+  { path: 'logs', component: MessagesComponent }
 ];
 @NgModule({
   exports: [ RouterModule ],
